d5dab3ae86607831f961070772bf5c5ede06ab27
Andrea Carter <40808764+mirtacarter@users.noreply.github.com>
2018-07-25 16:53:19 -0400

    added counter, show/hide elements

---
+++ assets/javascript/app.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+// Global variables
+//=======================================================================================    
+
+    //Counter variables
+    var number = 120;
+    var intervalId;
+
+
+// Counter Functions
+//=======================================================================================   
+    //Start button begins game clock, hides itself, and shows the quiz accordion
+    $("#startbutton").on("click", function (){
+        $(this).hide();
+        $(".counter").show();
+        $(".gamewrapper").show();
+        startClock();
+    });
+
+
+    // Counter will decrement by 1 second when game is started
+    function startClock() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    //  The decrement function.
+    function decrement() {
+
+      //  Decrease number by one.
+      number--;
+
+      //  Show the counter on the page
+      $(".counter").html("<h2>" + number + "</h2>");
+
+
+      //  Once number hits zero...
+      if (number === 0) {
+
+        //  ...run the stop function.
+        stop();
+
+        //  Alert the user that time is up.
+        alert("Time Up!");
+      }
+    }
+
+
+    function stop() {
+        //  Clears our intervalId
+        clearInterval(intervalId);
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
